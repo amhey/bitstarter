0522f34cdb2ad527c8b814397dfa74af9b5cc765
Angela Hey <amhey@heymash.com>
2013-07-10 20:53:48 +0000

    Had to require(fs) added this

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 //This is the file reading section
+var fs=require("fs");
 var filetoread="index.html";
 var filecontent = fs.readFileSync(filetoread,"utf8");
+
 
 app.get('/', function(request, response) {
   response.send(filecontent);
@@ -14,3 +16,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
